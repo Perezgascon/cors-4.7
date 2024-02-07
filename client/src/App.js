9c4cc6ad01b6fd82e79a286399f6dc4b8df5ae05
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-07 12:32:20 +0700

    Created react app

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import FruitManager from './components/FruitManager'
+
+
+export default function App() {
+  return (
+    <div><FruitManager /></div>
+  )
+}
---
+++ client/src/components/FruitManager.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function FruitManager() {
+    const [fruits, setFruits] = ([]);
+    const [loading, setLoading] = (false);
+
+    useEffect(() => {
+        fetchFruits()
+    }, [])
+
+    const fetchFruits = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.get('http://localhost:8080/fruits')
+            setFruits(response.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Fruits</h1>
+            <ul>
+                {fruits.map(fruit => (
+                    <li key={fruit.id}>{fruit.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
